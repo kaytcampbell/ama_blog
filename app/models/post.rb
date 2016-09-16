5ddbd052ed4b8b1efe613dd662bd8d79fc89cfc9
Kayt Campbell <kayt.campbell@ama.ab.ca>
2016-09-16 14:28:29 -0600

    Adding large size image option

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :photo, styles: { medium: "300x300>", large: "1000x1000>", thumb: "100x100>" }
   validates_attachment :photo, content_type: { content_type: /\Aimage\/.*\Z/ }
   has_many :popular_posts
 end
