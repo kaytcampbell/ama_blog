8320d285875de00312da5e06b5faa0415e590fbe
Kayt Campbell <kayt.campbell@ama.ab.ca>
2016-10-06 17:03:58 -0600

    removing the ability to register users from the ui

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # add back :registerable to add users from the ui
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
 end
